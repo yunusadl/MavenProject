10278b2b1db8741a07d8596fe093dae650844459
yunusadl <yunusadilemre@gmail.com>
2021-12-18 13:54:45 +0100

    Started Hello Feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -6,6 +6,10 @@ public class Hello {
 
         System.out.println("Developing Hello Feature");
 
+        System.out.println("Local Repo is already published to GitHub");
+
+        System.out.println("Doing more work");
+
 
     }
 }
